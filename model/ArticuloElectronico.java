5feb9cf1d4bfc0bc155dc633bbcdc3555aa6953d
Your Name <acolllen@myuax.com>
2025-12-11 18:19:28 +0100

    Clase de ArticuloElectronico realizada

---
+++ model/ArticuloElectronico.java
@@ -0,0 +1,26 @@
+package model;
+
+public abstract class ArticuloElectronico implements Mostrable {
+
+    protected String nombre;
+    protected double precio;
+    protected Marca marca;
+
+    public ArticuloElectronico(String nombre, double precio, Marca marca) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.marca = marca;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+}
