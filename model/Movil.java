3aded51e9b413f1e37dd3ab2921e0b15f6544d5c
Your Name <acolllen@myuax.com>
2025-12-11 23:22:36 +0100

    corrección en la clase Movil y en el main

--- model/Movil.java
+++ model/Movil.java
@@ -3,18 +3,26 @@ package model;
 public class Movil extends ArticuloElectronico {
 
     private int ramGB;
+    private double pantallaPulgadas;
     private String sistemaOperativo; // Android o iOS
 
-    public Movil(String nombre, double precio, Marca marca, int ramGB, String sistemaOperativo) {
+    public Movil(String nombre, double precio, Marca marca,
+                 int ramGB, double pantallaPulgadas, String sistemaOperativo) {
+
         super(nombre, precio, marca);
 
-        sistemaOperativo = sistemaOperativo.toUpperCase();
+        if (ramGB <= 0)
+            throw new IllegalArgumentException("La RAM debe ser mayor que 0");
 
-        if (!sistemaOperativo.equals("ANDROID") && !sistemaOperativo.equals("IOS")) {
-            throw new IllegalArgumentException("Sistema operativo inválido.");
-        }
+        if (pantallaPulgadas <= 0)
+            throw new IllegalArgumentException("Las pulgadas deben ser mayores que 0");
+
+        if (!sistemaOperativo.equalsIgnoreCase("Android") &&
+                !sistemaOperativo.equalsIgnoreCase("iOS"))
+            throw new IllegalArgumentException("Sistema operativo no válido");
 
         this.ramGB = ramGB;
+        this.pantallaPulgadas = pantallaPulgadas;
         this.sistemaOperativo = sistemaOperativo;
     }
 
@@ -23,7 +31,8 @@ public class Movil extends ArticuloElectronico {
         System.out.println("Móvil: " + nombre +
                 " | Precio: " + precio +
                 " | Marca: " + marca.getNombre() +
-                " | RAM: " + ramGB +
-                "GB | SO: " + sistemaOperativo);
+                " | RAM: " + ramGB + "GB" +
+                " | Pantalla: " + pantallaPulgadas + "''" +
+                " | SO: " + sistemaOperativo);
     }
 }
